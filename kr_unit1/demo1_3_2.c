a6929c59c21ac628f016fca2732219ffb1299465
Jacky Yang <xinle.yang@gmail.com>
2011-12-21 21:52:31 +0800

    init the repository

---
+++ kr_unit1/demo1_3_2.c
@@ -0,0 +1,31 @@
+/**
+ *
+ * File: demo1_3_2.c
+ * Author: Jacky Yang
+ * Date: 2011-12-21
+ * Desc: 
+ *    better written for word counting.
+ */
+#include <stdio.h>
+
+#define OUT 0
+#define IN 1
+
+int main()
+{
+    char c;
+    int state,nc;
+    state = OUT;
+    nc = 0;
+    while(c=getchar()!='*') {
+        printf("%08x \n",c);
+        if(c=='\n'||c==' '||c=='\t'||c==EOF) {
+            state = OUT;
+        } else if(state==OUT) {
+            state = IN;
+            nc++;
+        }
+    }
+    printf("count=[%d]",nc);
+    return 0;
+}
