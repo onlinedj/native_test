8dc221a09cddabbd5fb976878d000c7982963d34
yangxinle <xinle.yang@nufront.com>
2012-11-22 18:42:04 +0800

    update test

--- test/time.c
+++ test/time.c
@@ -1,9 +1,32 @@
+/*=============================================================================
+#     FileName: time.c
+#         Desc: test time related function
+#       Author: Jacky
+#        Email: xinle.yang@gmail.com
+#     HomePage: yangxinle.com
+#      Version: 0.0.1
+#   LastChange: 2012-11-21 15:19:49
+#      History:
+=============================================================================*/
+
 #include <stdio.h>
 #include <time.h>
 #include <sys/time.h>
 #include <stdlib.h>
+int test1();
+int test2();
 
 int main(int argc, const char *argv[])
+{
+}
+
+int test2()
+{
+    
+
+}
+
+int test1()
 {
     struct timeval val1;
     //struct timeval val2;
